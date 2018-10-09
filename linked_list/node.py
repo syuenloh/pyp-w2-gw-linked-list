6861218f59c256c5e25d3587b7e477265b5b49a5
Syuen Loh <syuenloh.ds@gmail.com>
2018-10-09 12:05:42 +0800

    complete node.py code

--- linked_list/node.py
+++ linked_list/node.py
@@ -3,21 +3,27 @@ class Node(object):
     Node class representing each of the linked nodes in the list.
     """
 
-    def __init__(self, elem):
+    def __init__(self, elem,next=None):
         self.elem=elem
-        self.next=None
+        self.next=next
         
     def __str__(self):
-        if self.next != None:
-            return "Node({}) > Node ({})".format(self.elem,self.next)
-        else:
+        if self.next==None:
             return "Node({}) > /".format(self.elem)
+        else:
+            return "Node({}) > Node({})".format(self.elem,self.next.elem)
 
     def __eq__(self, other):
-        return self.elem==other.elem
+        if self is other:
+            return True
+        elif type(self) != type(other):
+            return False
+        else:
+            return self.elem==other.elem
 
     def __repr__(self):
-        if self.next != None:
-            return "Node({}) > Node ({})".format(self.elem,self.next)
-        else:
+        if self.next==None:
             return "Node({}) > /".format(self.elem)
+        else:
+            return "Node({}) > Node({})".format(self.elem,self.next.elem)
+        
