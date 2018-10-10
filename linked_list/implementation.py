4c0344001b95ac817068965e68b852b3731c2888
Syuen Loh <syuenloh.ds@gmail.com>
2018-10-10 18:26:14 +0800

    attempt #2

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -3,11 +3,15 @@ class LinkedList():
     Implementation of an AbstractLinkedList inteface.
     """
 
-    def __init__(self, elements=None):
-        self.start=elements[0]
-        self.end=elements[-1]
+    def __init__(self,elements=None):
         self.elements=elements
-        return self
+        self.start = None
+        self.end = None
+        if elements:
+            self.start=Node(elements[0],next=Node(elements[1]))
+            self.end=Node(elements[-1])
+            for elem in elements:
+                self.append(elem)
 
     def __str__(self):
         return str(self.elements)
@@ -19,7 +23,7 @@ class LinkedList():
         pass
 
     def __getitem__(self, index):
-        pass
+        return self.elements[index]
 
     def __add__(self, other):
         return self.elements.append(other.elements)
@@ -28,14 +32,29 @@ class LinkedList():
         return self.elements.append(other.elements)
 
     def __eq__(self, other):
-        return all(x==y for x,y in zip(self.elements,other.elements))
-
+        if self is other:
+            return True
+        elif type(self) != type(other):
+            return False
+        else:
+            return self.elements==other.elements
+        
     def append(self, elem):
-        self.elements=elements.append(elem)
-        return self
+        new_node = Node(elem)
+        if self.start is None:
+            self.start = new_node
+            self.end = self.start
+            self.elements=[elem]
+        else:
+            self.end.next = new_node
+            self.end = new_node
+            self.elements.append(elem)
 
     def count(self):
-        return count(self)
+        return len(self.elements)
+    
+    def __repr__(self):
+        return "LinkedList({})".format(self.elements)
 
     def pop(self, index=None):
         if index==None:
